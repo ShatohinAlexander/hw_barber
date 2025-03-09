590bc0b04b35a0d95c7fe1e0017b24fab6f51fef
ShatohinAlexander <alexander.shatohin95@mail.ru>
2025-03-10 01:54:40 +0700

    админка

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Master, Service, Visit
+
+@admin.register(Master)
+class MasterAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "contact_info")
+
+@admin.register(Visit)
+class VisitAdmin(admin.ModelAdmin):
+     list_display = (
+        "name",
+        "phone",
+        "created_at",
+        "status",
+        "master",
+    )
+     
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ("name", "price")
