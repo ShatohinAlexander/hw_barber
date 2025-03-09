5002969728597875a722bc0b30724d056934a1ef
ShatohinAlexander <alexander.shatohin95@mail.ru>
2025-03-09 23:50:03 +0700

    создание представлений и подключение статики

--- barber/urls.py
+++ barber/urls.py
@@ -15,8 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+from main.views import MainPageView
+
+urlpatterns = (
+    [
+        path("admin/", admin.site.urls),
+        path('', MainPageView.as_view(), name='main_page'),
+    ]
+    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+)
--- main/views.py
+++ main/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView, ListView, TemplateView
 
-# Create your views here.
+from main.forms import ReviewForm, VisitForm
+from main.models import Master, Service, Visit
+
+MENU = [
+    {"title": "Главная", "url": reverse_lazy("main:main_page")},
+    {'title': 'Мастера', 'url': '#masters'},
+    {'title': 'Услуги', 'url': '#services'},
+    {'title': 'Записи', 'url': reverse_lazy("main:visit_list")},
+    {'title': 'Отзыв', 'url': reverse_lazy("main:review_create")},
+    {'title': 'Запись на стрижку', 'url': '#orderForm'},
+]
+
+class MainPageView(CreateView):
+    template_name = "main/index.html"
+    form_class = VisitForm
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["menu"] = MENU
+        context["masters"] = Master.objects.all()
+        context['services'] = Service.objects.all()
+
+        return context
+    
+class ThanksView(TemplateView):
+    template_name = 'main/thanks.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["menu"] = MENU
+        return context
+    
