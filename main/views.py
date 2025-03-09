e25b20d18a6fb24fa093e1c3dff237035d7ace89
ShatohinAlexander <alexander.shatohin95@mail.ru>
2025-03-10 01:56:11 +0700

    создание представлений и шаблона thanks

--- main/views.py
+++ main/views.py
@@ -1,16 +1,13 @@
-from django.shortcuts import redirect, render
-from django.urls import reverse, reverse_lazy
-from django.views.generic import CreateView, ListView, TemplateView
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, TemplateView
 
-from main.forms import ReviewForm, VisitForm
-from main.models import Master, Service, Visit
+from main.forms import VisitForm
+from main.models import Master, Service
 
 MENU = [
-    {"title": "Главная", "url": reverse_lazy("main:main_page")},
+    {"title": "Главная", 'url': reverse_lazy("main_page")},
     {'title': 'Мастера', 'url': '#masters'},
     {'title': 'Услуги', 'url': '#services'},
-    {'title': 'Записи', 'url': reverse_lazy("main:visit_list")},
-    {'title': 'Отзыв', 'url': reverse_lazy("main:review_create")},
     {'title': 'Запись на стрижку', 'url': '#orderForm'},
 ]
 
